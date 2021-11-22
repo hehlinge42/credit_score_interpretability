4feb8f6a4a78c5b8ad668e1f39e6b7b20b619e54
Adrien CATUSSE <adrien.catusse@gmail.com>
2021-11-22 22:42:56 +0100

    Formatted code with Black

--- src/own_model.py
+++ src/own_model.py
@@ -6,6 +6,7 @@ from sklearn.compose import make_column_transformer
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from joblib import dump
 
+
 class OwnClassifierModel:
     def __init__(self, config) -> None:
         self.config = config["data"]
@@ -25,12 +26,11 @@ class OwnClassifierModel:
     def train_model(self) -> None:
         logger.debug(f"Initializing {self.__class__.__name__}")
 
-        self.X_train = self.data[self.data["y_hat"].isna()].reset_index(drop=True) 
-        #TODO: put in config file for y_hat (cleaner)
+        self.X_train = self.data[self.data["y_hat"].isna()].reset_index(drop=True)
+        # TODO: put in config file for y_hat (cleaner)
         y_train = self.X_train[self.prediction_column]
         self.X_train.drop(columns=[self.prediction_column, "y_hat"], inplace=True)
-        self.X_test = self.data.dropna(axis='index') \
-                                .reset_index(drop=True)
+        self.X_test = self.data.dropna(axis="index").reset_index(drop=True)
         self.y_test = self.X_test[self.prediction_column]
         logger.debug(f"Separated train from test datasets")
 
@@ -44,32 +44,42 @@ class OwnClassifierModel:
         dump(self.model, self.model_path)
         logger.debug(f"Model saved")
 
-
-    def analyze_model_perfs(self)-> None: #TODO: decide which outputs we want and what we do with them
+    def analyze_model_perfs(
+        self,
+    ) -> None:  # TODO: decide which outputs we want and what we do with them
         logger.debug(f"Initializing {self.__class__.__name__}")
         y_pred = self.model.predict(self.X_test_preprocessed)
         logger.debug(f"Prediction done")
         print(self.y_test)
         print(y_pred)
-        acc_score = accuracy_score(self.y_test, y_pred) #TODO: need to save it somewhere ?
-        conf_matrix = confusion_matrix(self.y_test, y_pred) #TODO: need to save it somewhere ?
-        class_report = classification_report(self.y_test, y_pred) # My favourite one
+        acc_score = accuracy_score(
+            self.y_test, y_pred
+        )  # TODO: need to save it somewhere ?
+        conf_matrix = confusion_matrix(
+            self.y_test, y_pred
+        )  # TODO: need to save it somewhere ?
+        class_report = classification_report(self.y_test, y_pred)  # My favourite one
         logger.debug(f"Classification report obtained")
         print(class_report)
 
-    def make_prediction(self) ->None:
+    def make_prediction(self) -> None:
         logger.debug(f"Initializing {self.__class__.__name__}")
         y_pred_scores = self.model.predict_proba(self.X_test_preprocessed)
         logger.debug(f"Scores obtained")
-        self.X_test["y_hat_own_model"] = y_pred_scores[:,1]
+        self.X_test["y_hat_own_model"] = y_pred_scores[:, 1]
 
-        self.X_test.to_csv(self.output_data_path, sep=';', index=False)
+        self.X_test.to_csv(self.output_data_path, sep=";", index=False)
         logger.debug(f"Data exported")
 
     def _preprocess_data(self) -> None:
         logger.debug(f"Initializing {__class__.__name__}")
         pipeline_preprocessing = make_column_transformer(
-            (OrdinalEncoder(), self.categorical_features) #TODO: check whether we want one-hot encoder instead (risk of fucking with SHAP and other methods)
-        ) #TODO: check whether we need other pre-processing steps (NAs, etc.)
+            (
+                OrdinalEncoder(),
+                self.categorical_features,
+            )  # TODO: check whether we want one-hot encoder instead (risk of fucking with SHAP and other methods)
+        )  # TODO: check whether we need other pre-processing steps (NAs, etc.)
         self.X_train_preprocessed = pipeline_preprocessing.fit_transform(self.X_train)
-        self.X_test_preprocessed = pipeline_preprocessing.transform(self.X_test.drop(columns=[self.prediction_column, "y_hat"]))
+        self.X_test_preprocessed = pipeline_preprocessing.transform(
+            self.X_test.drop(columns=[self.prediction_column, "y_hat"])
+        )
